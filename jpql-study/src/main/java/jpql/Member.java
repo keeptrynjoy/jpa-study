ae2e9699a41202d6dec7877881bd4aa1c12cc270
keeptrynjoy <sungmin4218@gmail.com>
2023-03-05 11:09:43 +0900

    JPQL Study - TypeQuery, Query

---
+++ jpql-study/src/main/java/jpql/Member.java
@@ -0,0 +1,50 @@
+package jpql;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+    @Id @GeneratedValue
+    private Long id;
+    private String userName;
+    private int age;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Member() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
